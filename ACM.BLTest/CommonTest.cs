afa49c70e28a0152911c2a6a53c6b245bfaea3f6
Jordy Gierveld <GierveldJ@delagelanden.com>
2020-03-05 09:25:35 +0100

    Added common class

---
+++ ACM.BLTest/CommonTest.cs
@@ -0,0 +1,26 @@
+﻿using ACM.BL;
+using System;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+using Acme.Common;
+
+namespace ACM.BLTest
+{
+    public class CommonTest
+    {
+        [Fact]
+        public void TestStringHandler()
+        {
+            //Arrange
+            string expected = "Screw Driver";
+
+            //Act
+            string setup = "ScrewDriver";
+            string actual = setup.ReturnCleanString();
+
+            //Result
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Acme.Common/StringHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Acme.Common
+{
+    public static class StringHandler
+    {
+        public static string ReturnCleanString(this string fileString)
+        {
+            string result = string.Empty;
+
+            foreach (char letter in fileString)
+            {
+                if (letter == char.ToUpper(letter))
+                {
+                    result += " ";
+                }
+
+                result += letter;
+            }
+
+            return result.Trim();
+        }
+    }
+}
